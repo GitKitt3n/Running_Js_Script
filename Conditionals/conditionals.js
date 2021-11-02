dc46c3464d90f3e8b4ef3acd9e7a49c926a3ef2c
Karen Marshall <rockieriot13@gmail.com>
2021-11-01 21:58:40 -0700

    Added Conditionals and moved folders around. Also did some renaming

---
+++ Conditionals/conditionals.js
@@ -0,0 +1,24 @@
+let random = Math.random();
+
+if (1 + 1 === 2) {
+  console.log("YOU WIN!");
+}
+
+if (random > 10) {
+  console.log("TRY AGAIN");
+  console.log(random);
+}
+
+// ELSE IFs
+
+const dayOfWeek = "Friday";
+
+if (dayOfWeek === "Friday") {
+  console.log("Freedom!!!!!");
+} else if (dayOfWeek === "Monday") {
+  console.log("MANIC MONDAY!");
+} else if (dayOfWeek === "Tuesday") {
+  console.log("Turbulent Tuesdays");
+} else {
+  console.log("Work sucks, I know...");
+}
