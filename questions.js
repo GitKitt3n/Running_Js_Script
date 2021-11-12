7b0e41c7105e1a0d210823d453f702975802543c
Karen Marshall <rockieriot13@gmail.com>
2021-11-12 14:48:52 -0800

    More work with Loops

--- questions.js
+++ questions.js
@@ -1,7 +1,12 @@
 //when to use an object in an array vs an array in an object?
+if need specific information associate with product best to use Object
+
+big list of things ARRAY 
+
+
 
 //Ask Will to explain this
-
+//if statement is true go into my block
 const num = 102;
 
 if (num <= 100) {
@@ -19,6 +24,19 @@ if (num <= 100) {
 //difference between + and +=
 //how to not crash the browser when making for loops
 
-for(let i = 25; i<= 25; -= 5) {
-  console.log(i);
+//for(let i = 25; i<= 25;  i -= 5) {
+  //console.log(i);
+}//wrong
+
+//for(let i = 25; i >= 0; i -= 5) {
+  //console.log(i);
+//}
+
+//11.12.21 Trouble understanding the argument in loops why using index i in console
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for(let i = 0; i < people.length; i++){
+    console.log(people[i].toUpperCase());
 }
